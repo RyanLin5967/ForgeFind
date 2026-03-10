7bc22306ed866288ac0a399961b83204f3f425a7
RyanLin5967 <idident5967@gmail.com>
2026-03-10 11:53:12 -0400

    initial deploy

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -5,7 +5,7 @@ let score = null;
 export async function send_image(file){
     const formData = new FormData();
     formData.append("image", file);
-    const response = await fetch("https://forgefind.onrender.com/upload", {
+    const response = await fetch("https://yourusername-forgefind.hf.space/upload", {
         method: "POST",
         body: formData,
     });
